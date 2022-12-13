cc8c9888e0b1862559e2b441f96c714839defa0a
Filipe Magalhães Moreira <filipe.mm.1999@hotmail.com>
2022-12-12 22:27:50 -0300

    add Ex2json

---
+++ Ex2json/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Text.Json;
+
+namespace Ex2json
+{
+    class Program
+    {
+        public static void Main(string[] args)
+
+        {
+            
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\filip\OneDrive\Área de Trabalho\.Net\Ex2json\arquivosJson\");
+            foreach (FileInfo f in di.GetFiles())
+            {
+                if (f.Extension.ToLower() == ".json")
+
+                {
+                    using (StreamReader r = new StreamReader($"{f}"))
+                    {
+                        string json = r.ReadToEnd();
+                        var cliente = JsonSerializer.Deserialize<Cliente>(json, new JsonSerializerOptions
+                        {
+                            PropertyNameCaseInsensitive = true
+                        });
+                        Console.WriteLine($"Nome: {cliente.Nome} - Telefone: {cliente.Telefone}");
+                    }
+
+                }
+
+            }
+
+
+
+        }
+        
+
+    }
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
